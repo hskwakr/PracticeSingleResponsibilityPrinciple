5debdcc5df4056c6f992f2eae2e383dff6f09ea5
hskwakr <33633391+hskwakr@users.noreply.github.com>
2020-11-08 12:03:13 +0900

    Implement AfterEventDrivenArchitecture

---
+++ PracticeSingleResponsibilityPrinciple/Sample1/AfterEventDrivenArchitecture/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace PracticeSingleResponsibilityPrinciple.Sample1.AfterEventDrivenArchitecture
+{
+    /// <summary>
+    /// This class is temporary class to run the sample code.
+    /// </summary>
+    public class Order
+    {
+        public string to;
+        public string from;
+        public string body;
+    }
+}
---
+++ PracticeSingleResponsibilityPrinciple/Sample1/AfterEventDrivenArchitecture/OrderService.cs
@@ -0,0 +1,20 @@
+﻿namespace PracticeSingleResponsibilityPrinciple.Sample1.AfterEventDrivenArchitecture
+{
+    public class OrderService : IOrderService
+    {
+        private IOrderRepository _repository;
+
+        public OrderService(IOrderRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Create()
+        {
+            // fill the Order entity using the full power of our Business Logic(discounts, promotions, etc.)
+            var order = new Order();
+
+            _repository.Save(order);
+        }
+    }
+}
