c7d0e7f2246cd725aedb748558df8ff34d9064ee
hskwakr <33633391+hskwakr@users.noreply.github.com>
2020-11-23 04:28:26 +0900

    Implement a sample2 of before SRP

---
+++ PracticeSingleResponsibilityPrinciple/Sample2/BeforeSRP/WorkReport.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PracticeSingleResponsibilityPrinciple.Sample2.BeforeSRP
+{
+    public class WorkReport
+    {
+        private readonly List<WorkReportEntry> _entries;
+
+        public WorkReport()
+        {
+            _entries = new List<WorkReportEntry>();
+        }
+
+        public void AddEntry(WorkReportEntry entry) => _entries.Add(entry);
+        public void RemoveEntry(int index) => _entries.RemoveAt(index);
+
+        public void SaveToFile(string directoryPath, string fileName)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            File.WriteAllText(Path.Combine(directoryPath, fileName), ToString());
+        }
+
+        public override string ToString() =>
+            string.Join(
+                Environment.NewLine,
+                _entries.Select(x => $"Code: {x.ProjectCode}, Name:{x.ProjectName}, Hours:{x.SpentHours}")
+            );
+    }
+}
---
+++ PracticeSingleResponsibilityPrinciple/Sample2/BeforeSRP/WorkReportEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace PracticeSingleResponsibilityPrinciple.Sample2.BeforeSRP
+{
+    public class WorkReportEntry
+    {
+        public string ProjectCode { get; set; }
+        public string ProjectName { get; set; }
+        public string SpentHours { get; set; }
+    }
+}
